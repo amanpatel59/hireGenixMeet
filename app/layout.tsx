0e82ffb1003742eac30383e6fabe00f1098e7dd2
Aman Patel <ap707359@gmail.com>
2025-04-12 23:57:51 +0530

    Configure hireGenix Meet

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "hireGenix",
   description: "Video calling App",
   icons: {
-    icon: "/icons/logo.svg",
+    icon: "/icons/hireGenix logo.svg",
   },
 };
 
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/hireGenix logo.svg",
           },
           variables: {
             colorText: "#fff",
